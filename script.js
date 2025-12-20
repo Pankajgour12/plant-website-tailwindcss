eff4bbd5338190d30f46ccf162134523591e2876
pankajgour12 <pankajgour5000@gmail.com>
2025-12-20 11:58:18 +0530

    Review Section =================

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const navMenu = document.getElementById('nav-menu')
 
 const navlinks = document.querySelectorAll('.link')
@@ -16,3 +17,45 @@ navlinks.forEach(link => {
         humburger.classList.toggle('ri-close-large-line')
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//? Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+   speed:400,
+   spaceBetween:30,
+   autoplay:{
+    delay:3000,
+    disableOnInteraction:false
+   },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true
+  },
+  grabCursor:true,
+  breakpoints:{
+    640:{
+        slidesPerView:1},
+
+    768:{
+        slidesPerView:2},
+    1024:{
+        slidesPerView:3}
+  ,
+    }
+ 
+});
